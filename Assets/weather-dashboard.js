6ffd21b936e5ba9bf2d1eafafd64c802f7d453d3
Fabian <fabianffc@gmail.com>
2020-11-14 00:26:03 -0600

    curren-city,wind,temp results

--- Assets/weather-dashboard.js
+++ Assets/weather-dashboard.js
@@ -1,21 +1,16 @@
+//Pull the selector for the html to JS and assign them into a variable
 const cityName = document.querySelector("#city-name");
 const searchButton = document.querySelector("#search-button");
 const clearHistory = document.querySelector("#clear-history");
 const currentCity = document.querySelector("#current-city");
 const temp = document.querySelector("#temperature");
-const humidity = document.querySelector("#humidity");
+const description = document.querySelector("#description");
 const windSpeed = document.querySelector("#wind-speed");
 const uvIndex = document.querySelector("#UV-Index");
 
 //This is our APIKey for OpenWeather website
 const myAPIKey="d85ff1873d2dce2e6a1e8ef9f5812e12"
-// Here we are building the URL we need to query the database
-
-// Here we run our AJAX call to the OpenWeatherMap API
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
+//Make funtionality to our selectors
 searchButton.addEventListener("click",function(){
   
   queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName.value+"&units=imperial&appid=" + myAPIKey 
@@ -23,14 +18,16 @@ searchButton.addEventListener("click",function(){
     return response.json()
   }).then(function(results){
     console.log(results)
+    //on currentDay variable we switch the parameters from seconds to an actual understandable format
     var currentDay=moment(results.dt,"X").format("LLL")
     var iconcode=results.weather[0].icon
     var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
     var img="<img src="+iconurl+"    />"
     
-    
+    windSpeed.innerHTML= `Wind Speed: ${results.wind.speed} mph`
     currentCity.innerHTML=results.name +" " + currentDay+img
     temp.innerHTML= `Temperature: ${results.main.temp}°F`
+    description.innerHTML= results.weather[0].description
   })
   
   
@@ -46,8 +43,7 @@ searchButton.addEventListener("click",function(){
 
 
 })
-//WHEN I search for a city
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
+
 //WHEN I view current weather conditions for that city
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 //WHEN I view the UV index
